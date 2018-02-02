f246f90546fbe09b75d8a8814cc5b06429127dea
William Hammock <wghammock@gmail.com>
2018-02-01 23:37:45 -0800

    add secure tfvars file

--- corp-website.tf
+++ corp-website.tf
@@ -1,9 +1,7 @@
 # Specify the provider and access details
-# CLI will prompt user for region and keys
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+	region 						= "${var.region}"
+	shared_credentials_file 	= "/.aws/credentials"
 }
 
 
@@ -11,33 +9,33 @@ provider "aws" {
 # ===================================================
 
 resource "aws_default_vpc" "default" {
-    tags {
-        Name = "Default Free VPC"
-    }
+	tags {
+		Name = "Default Free VPC"
+	}
 }
 
 # Retrieve the AZ where we want to create network resources
 data "aws_availability_zone" "corporate-website" {
-  name = "${var.availability_zone}"
+	name = "${var.availability_zone}"
 }
 
 # ===================================================
 
 resource "aws_instance" "corporate-website" {
-  ami           = "${var.aws}"
-  instance_type = "t2.micro"
+	ami 			= "${var.aws_ami}"
+	instance_type 	= "t2.micro"
 
-    provisioner "local-exec" {
-    command = "echo ${aws_instance.corporate-website.public_ip} > ip_address.txt"
-  }
+	provisioner "local-exec" {
+		command 	= "echo ${aws_instance.corporate-website.public_ip} > ip_address.txt"
+	}
 }
 
 #Assign an elastic ip to the resource
 resource "aws_eip" "ip" {
-  instance = "${aws_instance.corporate-website.id}"
+	instance = "${aws_instance.corporate-website.id}"
 }
 
 #Output to CLI when done
 output "ip" {
-  value = "${aws_eip.ip.public_ip}"
+	value = "${aws_eip.ip.public_ip}"
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,7 @@
-# leave these blank, terraform will ask you for them
-variable "access_key" {}
-variable "secret_key" {}
-
 # Use North Virgina for great price and speed
-variable "region" = "us-east-1"
-variable "availability_zone" = "us-east-1f"
+variable "region" 	= "us-east-1"
+variable "az" 		= "us-east-1f"
 
-#Ubuntu Server 16.04 LTS (HVM), SSD Volume Type
-variable "aws_amis" = "ami-41e0b93b"
-
-variable "key_name" {
-  description = "Desired name of AWS key pair"
-}
+# Ubuntu Server 16.04 LTS 
+# (HVM), SSD Volume Type
+variable "aws_ami" 	= "ami-41e0b93b"
